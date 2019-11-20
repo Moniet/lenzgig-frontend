c651833f5f54a4e627e9abcc57b14b5a00adc6ce
_moniet <moniet@sugoilabs.com>
2019-11-20 14:45:13 +0530

    reusable flex component

---
+++ src/common/Flex/index.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+const FlexRow = styled.div`
+  display: flex;
+  flex-flow: ${({ nowrap }) => (nowrap ? 'row nowrap' : 'row wrap')};
+  justify-content: ${({ justifyLeft, justifyRight }) =>
+    justifyLeft ? 'flex-start' : justifyRight ? 'flex-end' : 'center'};
+  align-items: ${({ alignTop, alignBottom }) =>
+    alignTop ? 'flex-start' : alignBottom ? 'flex-end' : 'center'};
+`
+
+const FlexColumn = styled.div`
+  display: flex;
+  flex-flow: ${({ nowrap }) => (nowrap ? 'column nowrap' : 'column wrap')};
+  align-items: center;
+`
+
+export default props => {
+  const { direction, nowrap, children } = props
+  if (!direction || direction === 'row')
+    return <FlexRow {...props}>{children}</FlexRow>
+  if (direction === 'column')
+    return <FlexColumn {...props}>{children}</FlexColumn>
+}
