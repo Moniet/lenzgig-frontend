ebf05a43cbf89944d8368de0e412cdac78dab647
moniet <moniet.sawhney@gmail.com>
2020-01-07 01:03:04 +0530

    Updates

---
+++ precache-manifest.64dfa97097327f9b7344ef6fa20d008b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e94e0ead0f8c02aceec76ffcdcf15bfb",
+    "url": "/Moniet/lenzgig-frontend/index.html"
+  },
+  {
+    "revision": "406c658d88e6f8e049ba",
+    "url": "/Moniet/lenzgig-frontend/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "3515b303c6fb3a8ef725",
+    "url": "/Moniet/lenzgig-frontend/static/js/2.54d18199.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/Moniet/lenzgig-frontend/static/js/2.54d18199.chunk.js.LICENSE"
+  },
+  {
+    "revision": "406c658d88e6f8e049ba",
+    "url": "/Moniet/lenzgig-frontend/static/js/main.945d57fe.chunk.js"
+  },
+  {
+    "revision": "55c29b62bf118c8be940",
+    "url": "/Moniet/lenzgig-frontend/static/js/runtime-main.e1673198.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/Moniet/lenzgig-frontend/static/media/logo.25bf045c.svg"
+  }
+]);
